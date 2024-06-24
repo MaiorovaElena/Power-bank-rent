fc2393d6a4125c2ecaf88cd1bdde01ce2242ce6f
HelenRed <131916662+Chuchundra009@users.noreply.github.com>
2024-06-25 00:31:28 +0300

    add header structure

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,32 @@
 import st from './Header.module.css'
+import logo from '../../assets/logo.svg'
 
 export default function Header() {
   return (
-    <div className={st.header}>Header</div>
+    <div className={st.header}>
+      <div className={st.blokText}>
+        <div className={st.title}>
+          <h1 className={st.titleText}>Meteor</h1>
+        </div>
+        <nav className={st.menu}>
+          <ul className={st.menuList}>
+            <li className={st.menuLi}>benefits</li>
+            <li className={st.menuLi}>how to rent</li>
+            <li className={st.menuLi}>price</li>
+            <li className={st.menuLi}>faq</li>
+          </ul>
+        </nav>
+      </div>
+      <div className={st.rightBlock}>
+        <div className={st.language}>
+          <p className={st.languageEn}>EN</p>
+          <p>/</p>
+          <p className={st.languageSrb}>SRB</p>
+        </div>
+        <div className={st.logo}>
+          <a href="#"><img src={logo} className={st.logoImg} /></a>
+        </div>
+      </div>
+    </div>
   )
 }
