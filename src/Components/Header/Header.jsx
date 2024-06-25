f813f948330edfcc0f475e156bae6a41a6addf06
HelenRed <131916662+Chuchundra009@users.noreply.github.com>
2024-06-25 15:35:00 +0300

    add style header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,31 +1,35 @@
-import st from './Header.module.css'
-import logo from '../../assets/logo.svg'
+import st from './Header.module.css';
+import logo from '../../assets/logo.svg';
+// import { useState } from 'react';
+// import { NavLink } from 'react-router-dom';
 
 export default function Header() {
+  // const [classActive, setClassActive] = useState(false)
   return (
     <div className={st.header}>
       <div className={st.blokText}>
         <div className={st.title}>
-          <h1 className={st.titleText}>Meteor</h1>
+          <h1 className={st.titleText}><a href="" className={st.menuLink}>Meteor</a></h1>
         </div>
-        <nav className={st.menu}>
-          <ul className={st.menuList}>
-            <li className={st.menuLi}>benefits</li>
-            <li className={st.menuLi}>how to rent</li>
-            <li className={st.menuLi}>price</li>
-            <li className={st.menuLi}>faq</li>
-          </ul>
-        </nav>
+        <div className={st.textList}>
+          <nav className={st.menu}>
+            <ul className={st.menuList}>
+              <li className={st.menuLi}><a href="" className={`${st.menuLink} ${st.active}`}>benefits</a></li>
+              <li className={st.menuLi}><a href="" className={st.menuLink}>how to rent</a></li>
+              <li className={st.menuLi}><a href="" className={st.menuLink}>price</a></li>
+              <li className={st.menuLi}><a href="" className={st.menuLink}>faq</a></li>
+            </ul>
+          </nav>
+          <div className={st.language}>
+            <p className={st.languageEn}>EN</p>
+            <p className={st.slash}>/</p>
+            <p className={`${st.languageSrb} ${st.active}`}>SRB</p>
+          </div>
+        </div>
+
       </div>
-      <div className={st.rightBlock}>
-        <div className={st.language}>
-          <p className={st.languageEn}>EN</p>
-          <p>/</p>
-          <p className={st.languageSrb}>SRB</p>
-        </div>
-        <div className={st.logo}>
-          <a href="#"><img src={logo} className={st.logoImg} /></a>
-        </div>
+      <div className={st.logo}>
+        <a href="#" className={st.logoLink}><img src={logo} className={st.logoImg} /></a>
       </div>
     </div>
   )
