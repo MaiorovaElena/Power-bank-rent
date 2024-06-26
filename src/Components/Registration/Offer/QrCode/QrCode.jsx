9f7bd8b4851686440bef0ee18b02641fa08b5a20
Maria <zhukovamaria9@gmail.com>
2024-06-26 16:44:38 +0300

    Fix registration block, change fonts

--- src/Components/Registration/Offer/QrCode/QrCode.jsx
+++ src/Components/Registration/Offer/QrCode/QrCode.jsx
@@ -1,5 +1,6 @@
+import styles from "./qrCode.module.css";
 import image from "../../Images/QR.png";
 
 export default function QrCode() {
-  return <img src={image} alt="QR-code" />;
+  return <img className={styles.image} src={image} alt="QR-code" />;
 }
