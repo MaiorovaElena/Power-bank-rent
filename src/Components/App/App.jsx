d85c209eca1e8e78eefc6c9e8f53922b82c21405
HelenRed <131916662+Chuchundra009@users.noreply.github.com>
2024-07-01 21:39:18 +0300

    fix: centering the main container and adding the component DeviceRent

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,4 +1,5 @@
 import Benefits from "../Benefits/Benefits/Benefts";
+import DeviceRent from "../DeviceRent/DeviceRent";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import RegistrationSection from "../Registration/RegistrationSection/RegistrationSection";
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <RegistrationSection />
       <Benefits />
+      <DeviceRent/>
       <Footer />
     </div>
   );
---
+++ src/Components/DeviceRent/DeviceRent.jsx
@@ -0,0 +1,22 @@
+import st from './DeviceRent.module.css';
+import electricity from '../../assets/rent/electricity.gif';
+// import device from '../../assets/rent/rent-img.png';
+import deviceImg from '../../assets/rent/rentImg.png'
+
+export default function DeviceRent() {
+    return (
+        <div className={st.device}>
+            <div className={st.deviceTitle}>
+                <h2 className={st.titleLight}>Is your phone dying?</h2>
+                <h2 className={st.titleViolet}>Take the power bank!</h2>
+            </div>
+            <div className={st.images}>
+                <img src={deviceImg} className={st.deviceImg} alt="device rent" />
+                {/* <div className={st.electricityImg}> */}
+                    <img src={electricity} className={st.electricity} alt="electricity" />
+                {/* </div> */}
+
+            </div>
+        </div>
+    )
+}
