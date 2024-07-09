3ce005a600b606451e73dde07f1f722ba2ce4568
Elena Kosheleva <140403025+Eva71879@users.noreply.github.com>
2024-07-09 22:56:52 +0300

    add footer adaptive mobile

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,29 +1,64 @@
-import st from "./Footer.module.css";
+import React from "react";
+import st from "./Footer.module.css"; // импортируем стили Footer
 import logo from "../../assets/footer/meteorLogo.svg";
 import telegramIcon from "../../assets/footer/telegramIcon.png";
 import viberIcon from "../../assets/footer/viberIcon.png";
 
 const Footer = () => {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className={st.footer}>
       <div className={st.wrapper}>
         <h1 className={st.title}>Meteor</h1>
-        <img className={st.logo} src={logo} alt="logoImg" />
+        <img className={st.logo} src={logo} alt="logo Meteor" />
       </div>
       <nav className={st.nav}>
-        <a className={st.navLink} href="#benefits">
-          Benefits
-        </a>
-        <a className={st.navLink} href="#howToRent">
-          How to rent
-        </a>
-        <a className={st.navLink} href="#price">
-          Price
-        </a>
-        <a className={st.navLink} href="#faq">
-          FAQ
-        </a>
+        <ul className={st.navList}>
+          <li className={st.navItem}>
+            <a
+              className={st.navLink}
+              href="#benefits"
+              onClick={() => scrollToSection("benefits")}
+            >
+              Benefits
+            </a>
+          </li>
+          <li className={st.navItem}>
+            <a
+              className={st.navLink}
+              href="#howToRent"
+              onClick={() => scrollToSection("howToRent")}
+            >
+              How to rent
+            </a>
+          </li>
+          <li className={st.navItem}>
+            <a
+              className={st.navLink}
+              href="#price"
+              onClick={() => scrollToSection("price")}
+            >
+              Price
+            </a>
+          </li>
+          <li className={st.navItem}>
+            <a
+              className={st.navLink}
+              href="#faq"
+              onClick={() => scrollToSection("faq")}
+            >
+              FAQ
+            </a>
+          </li>
+        </ul>
       </nav>
+
       <div className={st.contacts}>
         <a className={st.phone} href="tel:+011XXXXXXXX">
           0 11 XXX XXXX
@@ -37,16 +72,23 @@ const Footer = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={telegramIcon} alt="Telegram Icon" />
+            <img
+              className={st.telegramIcon}
+              src={telegramIcon}
+              alt="Telegram Icon"
+            />
           </a>
           <a
             href="viber://chat?number=%2B011XXXXXXXX"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={viberIcon} alt="Viber Icon" />
+            <img className={st.viberIcon} src={viberIcon} alt="Viber Icon" />
           </a>
         </div>
+      </div>
+      <div className={st.logoMobileWrapper}>
+        <img className={st.logoMobile} src={logo} alt="logo" />
       </div>
     </div>
   );
