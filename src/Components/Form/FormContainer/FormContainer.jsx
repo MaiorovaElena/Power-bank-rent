fad320082a8846a03bcc684e609f2071c53a0a97
Maria <zhukovamaria9@gmail.com>
2024-07-18 00:19:08 +0300

    Add modal after form submission, add form data storage path

--- src/Components/Form/FormContainer/FormContainer.jsx
+++ src/Components/Form/FormContainer/FormContainer.jsx
@@ -1,18 +1,51 @@
 import Title from "../Title/Title";
 import styles from "./FormContainer.module.css";
 import Fields from "../Fields/Fields";
+import Modal from "react-modal";
+import { useState } from "react";
 
 export default function FormContainer() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const handleSubmit = () => {
+    openModal();
+  };
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const modalContent = (
+    <div>
+      Thank you for registering. This is a website prototype. We will send you a
+      promotional code as soon as we launch the service
+    </div>
+  );
+
   return (
     <div className={styles.container}>
       <Title />
+
       <form
         className={styles.form}
-        action="https://formspree.io/f/mqazknww"
+        action="https://api.form-data.com/f/rreze912qmcz88kgurxwn"
         method="POST"
+        onSubmit={handleSubmit}
       >
         <Fields />
       </form>
+      <Modal
+        className={styles.modal}
+        style={{ overlay: { zIndex: 100 } }}
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+      >
+        {modalContent}
+      </Modal>
     </div>
   );
 }
